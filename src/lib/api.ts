71f11c665772a2711a59be38938b689cc6075a97
eric <eric2004_24@hotmail.com>
2026-05-21 19:53:57 +0200

    feat: add comment to clarify usage of axios.get for fetching character data by URL

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -28,6 +28,7 @@ export async function GetFilmByID(id:string) {
 
 export async function getCharacterByURL(url:string){
     try{
+        // aqui esoty haciendo axios.get, no api.get, porque todo viene de urls completas, no relativas a la baseURL del api, entonces no puedo usar el api creado con axios.create, porque ese api ya tiene una baseURL definida, entonces uso axios.get directamente para hacer la petición a la url completa que me dan los datos de las pelis
         const response = await axios.get(url)
         return response.data
     }catch(error){
